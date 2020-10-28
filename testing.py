119cbe4d561aba1420abd8ee118f63fae4bc0179
kostyagladkov <kostya.gladkov.nn@gmail.com>
2020-10-28 19:42:06 +0200

    automated test for module

--- testing.py
+++ testing.py
@@ -1,16 +1,37 @@
 import caesar
-import pytest
+import unittest
 
 
-text = "test should be fine"
-key = 7
-
-def start():
-	caesar.encode(text,key)
+class Test(unittest.TestCase):
 
 
-def end():
-	caesar.decode(text,key)
+
+	def test_encode(self):
+
+		self.assertEqual(caesar.encode("abcd", -1), "zabc")
+		self.assertEqual(caesar.encode("abcd", 1), "bcde")
+		self.assertEqual(caesar.encode("nothing", -999), "cdiwxcv")	
+		self.assertEqual(caesar.encode("nothing", 999), "yzestyr")
+		self.assertEqual(caesar.encode("anything", 26), "anything")
+		self.assertEqual(caesar.encode("anything", -26), "anything")
+
+
+
+	def test_decode(self):
+
+		self.assertEqual(caesar.decode("zabc", -1), "abcd")
+		self.assertEqual(caesar.decode("bcde", 1), "abcd")
+		self.assertEqual(caesar.decode("cdiwxcv", -999), "nothing")	
+		self.assertEqual(caesar.decode("yzestyr", 999), "nothing")
+		self.assertEqual(caesar.decode("anything", 26), "anything")
+		self.assertEqual(caesar.decode("anything", -26), "anything")
+
+
+
+if __name__ == "__main__":
+	unittest.main()
+
+
 
 
 
