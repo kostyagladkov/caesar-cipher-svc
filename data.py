df0b5d07a3301e6207055b2ec824cc364b90ac4d
zerodeusx <zed14lvl@gmail.com>
2020-10-29 23:45:11 +0200

    database

--- data.py
+++ data.py
@@ -1,41 +1,39 @@
 import sqlite3
 import caesar
 
-conn = sqlite3.connect('cipher_database.db')
 
-c = conn.cursor()
+def db():
 
-c.execute("""CREATE TABLE cipher(encode text, decode text)""")
+	conn = sqlite3.connect('cipher_database.db')
 
-#For test
-#c.execute("INSERT INTO cipher VALUES ('abcd', 'bcde')")
+	c = conn.cursor()
+
+	c.execute("""CREATE TABLE cipher(encode text, decode text)""")
+
+	#For test
+	#c.execute("INSERT INTO cipher VALUES ('abcd', 'bcde')")
+
+	x1 = caesar.encode(text, key)
+
+	x2 = caesar.decode(text, key)
+
+	c.execute("INSERT INTO cipher VALUES (:encode, :decode)", {'encode': x1, 'decode': x2} )
+
+	conn.commit()
 
 
-x1 = caesar.encode("abcde", 1)
+	"""
+	----------------------
+	SHOW ALL TABLE CONTEXT
+	----------------------
 
-x2 = caesar.decode("bcdef", 1)
+	c.execute('SELECT * FROM cipher')
 
-c.execute("INSERT INTO cipher VALUES (:encode, :decode)", {'encode': x1, 'decode': x2} )
+	rows = c.fetchall()
 
-conn.commit()
-
-
-
-"""
-----------------------
-SHOW ALL TABLE CONTEXT
-----------------------
-
-c.execute('SELECT * FROM cipher')
-
-rows = c.fetchall()
-
-for row in rows:
+	for row in rows:
 
     print(row)
 
-"""
-
-
-
-conn.close()
+	"""
+	conn.close()
