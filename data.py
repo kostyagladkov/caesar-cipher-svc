3032e1a48ddc39c88ebf9d1a3667b241077e71e9
zerodeusx <zed14lvl@gmail.com>
2020-10-30 18:54:53 +0200

    database

--- data.py
+++ data.py
@@ -2,38 +2,57 @@ import sqlite3
 import caesar
 
 
-def db():
+def db_setter():
 
 	conn = sqlite3.connect('cipher_database.db')
 
 	c = conn.cursor()
 
-	c.execute("""CREATE TABLE cipher(encode text, decode text)""")
+	text = input("Write your text: ")
+	text = str(text)
 
-	#For test
-	#c.execute("INSERT INTO cipher VALUES ('abcd', 'bcde')")
+	key = input("Write your key: ")
+	key = int(key) 
 
-	x1 = caesar.encode(text, key)
+	x1 = caesar.encode(text,key)
 
-	x2 = caesar.decode(text, key)
+	x2 = caesar.decode(x1,key)
+
+	#Creating a table, if table is already created just comment or delete this line
+	#c.execute("""CREATE TABLE cipher(encode text, decode text)""")
 
 	c.execute("INSERT INTO cipher VALUES (:encode, :decode)", {'encode': x1, 'decode': x2} )
 
 	conn.commit()
-
-
-	"""
-	----------------------
-	SHOW ALL TABLE CONTEXT
-	----------------------
-
-	c.execute('SELECT * FROM cipher')
-
-	rows = c.fetchall()
-
-	for row in rows:
-
-    print(row)
-
-	"""
+	
 	conn.close()
+
+
+def db_getter():
+
+	conn = sqlite3.connect('cipher_database.db')
+
+	c = conn.cursor()
+
+	find_e = input("Write what do you want to find: ")
+	find_e = str(find_e)
+
+	c.execute("SELECT * FROM cipher WHERE ENCODE = '{}'".format(find_e))
+	print(c.fetchone())
+
+	print("Done")
+	#----------------------
+	#SHOW ALL TABLE CONTEXT
+	#----------------------
+	#c.execute('SELECT * FROM cipher ')
+	#rows = c.fetchall()
+	#for row in rows:
+	#	print(row)
+
+	#conn.commit()
+
+	conn.close()
+
+
+db_setter()
+db_getter()
